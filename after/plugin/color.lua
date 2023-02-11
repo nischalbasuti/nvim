707667b3ddedca51d8e32f4c11a69e02001d4eca
Nischal Basuti <nischalbasuti1@gmail.com>
2023-02-11 12:04:10 +0530

    Fix colors

--- after/plugin/color.lua
+++ after/plugin/color.lua
@@ -1,7 +1,5 @@
 -- Set colorscheme
 
--- vim.cmd('colorscheme gruvbox')
--- highlight Normal guibg=none ctermbg=none
 vim.o.termguicolors = true
 vim.cmd [[colorscheme onedark]]
-
+vim.o.cursorline = true
