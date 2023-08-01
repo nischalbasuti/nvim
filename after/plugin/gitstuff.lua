30132c5f24832982708ae445ba3e1129c106e45c
Nischal Basuti <nischalbasuti1@gmail.com>
2023-08-02 00:56:02 +0530

    map copilot to C-J, fix blamer

--- after/plugin/gitstuff.lua
+++ after/plugin/gitstuff.lua
@@ -1,13 +1,3 @@
--- Git blamer
-vim.g.blamer_enabled = 1
-vim.g.blamer_delay = 500
-vim.g.blamer_date_format = '20%y/%m/%d %H:%M'
-vim.g.blamer_relative_time = 1
-vim.g.blamer_show_in_visual_modes = 0
-vim.g.blamer_prefix = ' > '
-
--- vim.cmd [[highlight Blamer guifg=lightgrey]]
-
 -- Gitsigns
 -- See `:help gitsigns.txt`
 require('gitsigns').setup {
--- init.lua
+++ init.lua
@@ -256,3 +256,15 @@ vim.opt.colorcolumn = "81,121"
 --     api_key = "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"
 -- })
 
+-- Git blamer
+vim.g.blamer_enabled = 1
+vim.g.blamer_delay = 500
+vim.g.blamer_date_format = '20%y/%m/%d %H:%M'
+vim.g.blamer_relative_time = 1
+vim.g.blamer_show_in_visual_modes = 0
+vim.g.blamer_prefix = ' > '
+
+-- vim.cmd [[highlight Blamer guifg=lightgrey]]
+
+vim.g.copilot_no_tab_map = true
+vim.api.nvim_set_keymap("i", "<C-J>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
