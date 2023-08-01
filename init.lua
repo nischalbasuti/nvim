dd296be4194a5d7ff5ab9dcf5cd51a9f8a59c8d9
Nischal Basuti <nischalbasuti1@gmail.com>
2023-08-01 22:53:59 +0530

    move nvim-tree stuff to seperate file

--- init.lua
+++ init.lua
@@ -124,78 +124,6 @@ require('packer').startup(function(use)
   end
 end)
 
--- nvim-tree stuff
---
--- disable netrw at the very start of your init.lua (strongly advised)
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
-
--- set termguicolors to enable highlight groups
-vim.opt.termguicolors = true
-
-local HEIGHT_RATIO = 0.8  -- You can change this
-local WIDTH_RATIO = 0.5   -- You can change this too
-
-
-local function my_on_attach(bufnr)
-    local api = require "nvim-tree.api"
-
-    local function opts(desc)
-        return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
-    end
-
-    -- default mappings
-    api.config.mappings.default_on_attach(bufnr)
-
-    -- custom mappings
-    vim.keymap.set('n', '<C-t>', api.tree.change_root_to_parent,        opts('Up'))
-    vim.keymap.set('n', '?',     api.tree.toggle_help,                  opts('Help'))
-    vim.keymap.set('n', 'n',     api.fs.create,                  opts('New File'))
-    -- vim.keymap.set('n', 'u', api.fs.dir_up,        opts('Move Dir Up'))
-    vim.keymap.set( "n", "<Esc>", ":NvimTreeClose<CR>", opts('Close'))
-end
-
-require("nvim-tree").setup({
-    sort_by = "case_sensitive",
-    on_attach = my_on_attach,
-    view = {
-        float = {
-            enable = true,
-            open_win_config = function()
-                local screen_w = vim.opt.columns:get()
-                local screen_h = vim.opt.lines:get() - vim.opt.cmdheight:get()
-                local window_w = screen_w * WIDTH_RATIO
-                local window_h = screen_h * HEIGHT_RATIO
-                local window_w_int = math.floor(window_w)
-                local window_h_int = math.floor(window_h)
-                local center_x = (screen_w - window_w) / 2
-                local center_y = ((vim.opt.lines:get() - window_h) / 2)
-
-                - vim.opt.cmdheight:get()
-
-                return {
-                    border = 'rounded',
-                    relative = 'editor',
-                    row = center_y,
-                    col = center_x,
-                    width = window_w_int,
-                    height = window_h_int,
-
-                }
-            end,
-        },
-        width = function()
-            return math.floor(vim.opt.columns:get() * WIDTH_RATIO)
-        end,
-    },
-    renderer = {
-        group_empty = true,
-    },
-    filters = {
-        dotfiles = true,
-    },
-})
-
 -- When we are bootstrapping a configuration, it doesn't
 -- make sense to execute the rest of the init.lua.
 --
@@ -223,6 +151,7 @@ vim.o.smartindent = true
 vim.opt_local.shiftwidth = 4
 vim.opt_local.tabstop = 4
 vim.o.scrolloff = 4
+vim.opt.termguicolors = true -- set termguicolors to enable highlight groups
 
 vim.api.nvim_create_autocmd("FileType", {
     pattern = "javascript",
