2e7fc93f94c1cf90fe19f1d421aa41c49aca90b1
Nischal Basuti <nischalbasuti1@gmail.com>
2023-08-18 13:57:46 +0530

    add border to lsp help floating window

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -108,3 +108,17 @@ mason_lspconfig.setup_handlers {
 
 -- Turn on lsp status information
 require('fidget').setup()
+
+local _border = "single"
+
+vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(
+  vim.lsp.handlers.hover, {
+    border = _border
+  }
+)
+
+vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(
+  vim.lsp.handlers.signature_help, {
+    border = _border
+  }
+)
