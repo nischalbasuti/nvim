36cdd7703be2d895f11eee9b8c244532d4e326b6
Nischal Basuti <nischalbasuti1@gmail.com>
2023-10-23 00:17:06 +0530

    Add raindbow-delimiters (colored matching braces)

--- init.lua
+++ init.lua
@@ -118,6 +118,7 @@ require('packer').startup(function(use)
   use 'lukas-reineke/indent-blankline.nvim' -- Add indentation guides even on blank lines
   use 'numToStr/Comment.nvim' -- "gc" to comment visual regions/lines
   use 'tpope/vim-sleuth' -- Detect tabstop and shiftwidth automatically
+  use 'https://gitlab.com/HiPhish/rainbow-delimiters.nvim' -- Highlight matching parenthesis
 
   -- Fuzzy Finder (files, lsp, etc)
   use { 'nvim-telescope/telescope.nvim', branch = '0.1.x', requires = { 'nvim-lua/plenary.nvim' } }
