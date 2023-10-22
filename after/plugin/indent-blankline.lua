c6aae0178a81fd921883f19cab3c1aa2560bd88b
Nischal Basuti <nischalbasuti1@gmail.com>
2023-10-23 00:13:03 +0530

    bump

--- after/plugin/indent-blankline.lua
+++ after/plugin/indent-blankline.lua
@@ -13,20 +13,20 @@ vim.opt.termguicolors = true
 -- vim.cmd [[highlight IndentBlanklineIndent6 guifg=#C678DD gui=nocombine]]
 
 
-require('indent_blankline').setup {
-  -- char = '┊',
-  show_trailing_blankline_indent = false,
-  -- show_end_of_line = true,
-  -- space_char_blankline = " ",
-  show_current_context = true,
-  show_current_context_start = true,
-  -- char_highlight_list = {
-  --   "IndentBlanklineIndent1",
-  --   "IndentBlanklineIndent2",
-  --   "IndentBlanklineIndent3",
-  --   "IndentBlanklineIndent4",
-  --   "IndentBlanklineIndent5",
-  --   "IndentBlanklineIndent6",
-  -- },
+require('ibl').setup {
+  -- -- char = '┊',
+  -- show_trailing_blankline_indent = false,
+  -- -- show_end_of_line = true,
+  -- -- space_char_blankline = " ",
+  -- show_current_context = true,
+  -- show_current_context_start = true,
+  -- -- char_highlight_list = {
+  -- --   "IndentBlanklineIndent1",
+  -- --   "IndentBlanklineIndent2",
+  -- --   "IndentBlanklineIndent3",
+  -- --   "IndentBlanklineIndent4",
+  -- --   "IndentBlanklineIndent5",
+  -- --   "IndentBlanklineIndent6",
+  -- -- },
 }
 
